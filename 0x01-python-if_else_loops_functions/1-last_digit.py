18a422ac03bd6f86e66a6d3f09d3300785281e89
David <4292@holbertonschool.com>
2022-04-26 12:45:42 -0400

    grammar fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,11 +7,11 @@ else:
     bufnumber = number
 
 if bufnumber % 10 > 5:
-    print("The Last digit of {:d} is {:d} and is greater than 5"
+    print("Last digit of {:d} is {:d} and is greater than 5"
           .format(number, bufnumber % 10))
 elif number % 10 < 6 and number % 10 != 0:
-    print("The Last digit of {:d} is {:d} and is less than 6 and not 0"
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
           .format(number, bufnumber % 10))
 else:
-    print("The Last digit of {:d} is {:d} and is 0"
+    print("Last digit of {:d} is {:d} and is 0"
           .format(number, bufnumber % 10))
