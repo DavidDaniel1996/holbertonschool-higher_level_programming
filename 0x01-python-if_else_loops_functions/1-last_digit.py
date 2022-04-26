a93685ecd1999b96e03e12011a059fc5303feaf9
David <4292@holbertonschool.com>
2022-04-26 13:06:57 -0400

    fixed negative number error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 import random
+from re import L
 number = random.randint(-10000, 10000)
 if number < 0:
     bufnumber = number * -1
+    last_digit = (bufnumber % 10) * -1
 else:
     bufnumber = number
+    last_digit = bufnumber % 10
 
-if bufnumber % 10 > 5:
+if last_digit > 5:
     print("Last digit of {:d} is {:d} and is greater than 5"
-          .format(number, bufnumber % 10))
-elif number % 10 < 6 and number % 10 != 0:
+          .format(number, last_digit))
+elif last_digit % 10 < 6 and last_digit % 10 != 0:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0"
-          .format(number, bufnumber % 10))
+          .format(number, last_digit))
 else:
     print("Last digit of {:d} is {:d} and is 0"
-          .format(number, bufnumber % 10))
+          .format(number, last_digit))
