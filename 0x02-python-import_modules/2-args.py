f1c33ebeb05b320764aebbca3daf81775a5b4c05
David <4292@holbertonschool.com>
2022-04-28 12:56:48 -0400

    fix possible spacing issue

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if len(argv) == 2:
         count += 1
         print("{:d} argument:".format(len(argv) - 1))
-        print("{:d}: ".format(count), argv[count])
+        print("{:d}:".format(count), argv[count])
     else:
         if len(argv) == 1:
             print("{:d} arguments.".format(len(argv) - 1))
