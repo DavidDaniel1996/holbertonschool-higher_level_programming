3734b33407d8fc406342a97db4b2be197cb9405a
David <4292@holbertonschool.com>
2022-04-28 11:53:03 -0400

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sys import argv
+count = 0
+if len(argv) == 2:
+    count += 1
+    print("{:d} argument:".format(len(argv) - 1))
+    print("{:d}: ".format(count), argv[count])
+else:
+    if len(argv) == 1:
+        print("{:d} arguments.".format(len(argv) - 1))
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+        for i in range(0, len(argv) - 1):
+            count += 1
+            print("{:d}: ".format(count), argv[count])
