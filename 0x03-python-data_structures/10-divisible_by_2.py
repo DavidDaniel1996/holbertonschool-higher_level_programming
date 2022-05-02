cd122a877c3fbfbded8a26b9d32d7612af992636
David <4292@holbertonschool.com>
2022-05-02 13:03:56 -0400

    find multiple of 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = my_list[:]
+    idx = 0
+    for i in range(len(my_list)):
+        if my_list[idx] % 2 == 0:
+            new_list[idx] = True
+        else:
+            new_list[idx] = False
+        idx += 1
+    return new_list
