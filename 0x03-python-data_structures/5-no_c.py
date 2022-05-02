71458074e6fcc17148591154b804e04ef918a738
David <4292@holbertonschool.com>
2022-05-02 10:06:04 -0400

    remove c

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from email import message_from_binary_file
+
+
+def no_c(my_string):
+    idx = 0
+    new_string = ""
+    for i in range(len(my_string)):
+        if my_string[idx] == "c" or my_string[idx] == "C":
+            new_string = my_string[:idx] + my_string[idx + 1:]
+        idx += 1
+    return new_string
