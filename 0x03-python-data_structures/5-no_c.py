09b978df8b1bf24a645403f1640a660758ee0c01
David <4292@holbertonschool.com>
2022-05-03 15:18:23 -0400

    fix no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    idx = 0
-    new_string = ""
-    for i in range(len(my_string)):
-        if my_string[idx] == "c" or my_string[idx] == "C":
-            new_string = my_string[:idx] + my_string[idx + 1:]
-        idx += 1
-    return new_string
+
+    char_replace_lower = {'c': ''}
+    char_replace_upper = {'C': ''}
+
+    for i, value in char_replace_lower.items():
+        new_string1 = my_string.replace(i, value)
+    for i, value in char_replace_upper.items():
+        new_string2 = new_string1.replace(i, value)
+    return new_string2
