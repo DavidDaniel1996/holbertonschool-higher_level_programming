d408671b5d0f26226e90664c3aa8a208970ee74c
David <4292@holbertonschool.com>
2022-05-17 15:10:20 -0400

    get and set

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
-        if not type(size) is int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
@@ -13,6 +9,10 @@ class Square:
 
     @size.setter
     def size(self, value):
+        if not type(value) is int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
