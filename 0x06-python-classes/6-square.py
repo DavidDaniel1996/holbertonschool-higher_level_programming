fa59547437b543dca9007c4105f1afc583e4968d
David <4292@holbertonschool.com>
2022-05-17 16:48:38 -0400

    adding comments

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" Module contains class Square """
+
+
 class Square:
+    """ Defines Square """
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
