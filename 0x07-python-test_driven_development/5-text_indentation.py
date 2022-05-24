d2a22d108a40f7088fe76eb3edfd4c1a3df08a3e
David <4292@holbertonschool.com>
2022-05-24 09:45:43 -0400

    adding text indentation module

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module that indents a text based on special characters """
+
+
+def text_indentation(text):
+    """Function that prints indented text and an extra newline
+        Raises TypeError if text is not a string
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    special_chars = (".", "?", ":")
+
+    for i in special_chars:
+        text = text.replace(i, i + '\n')
+    text = text.replace('\n ', '\n')
+    text = text.replace('\n', '\n\n')
+    print(text, end="")
