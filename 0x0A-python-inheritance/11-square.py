31902c9aadd64cec912beb3e08e5c251d673f340
David <4292@holbertonschool.com>
2022-05-29 15:03:41 -0400

    added str square specific functionality

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module contains subclass Square """
+
+
+rect = __import__('9-rectangle').Rectangle
+
+
+class Square(rect):
+    """ subclass Square, of subclass Rectangle """
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+        rect.integer_validator(self, "size", self.__size)
+
+    def area(self):
+        result = self.__size * self.__size
+        return result
+
+    def __str__(self):
+        return (f"[Square] {self.__size}/{self.__size}")
