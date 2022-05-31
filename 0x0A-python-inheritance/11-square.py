aeaf87c535236d44941ade8896698904159c08e2
David <4292@holbertonschool.com>
2022-05-31 15:57:30 -0400

    fix

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -8,13 +8,9 @@ rect = __import__('9-rectangle').Rectangle
 class Square(rect):
     """ subclass Square, of subclass Rectangle """
     def __init__(self, size):
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-        rect.integer_validator(self, "size", self.__size)
-
-    def area(self):
-        result = self.__size * self.__size
-        return result
 
     def __str__(self):
         return (f"[Square] {self.__size}/{self.__size}")
