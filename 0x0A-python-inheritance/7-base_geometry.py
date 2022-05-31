046cbffe37704786b4a17e3eb381abc09bbe419b
David <4292@holbertonschool.com>
2022-05-31 15:32:52 -0400

    change method out of order

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,11 +4,11 @@
 
 class BaseGeometry:
     """ Defines class BaseGeometry """
+    def area(self):
+        raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         if type(value) not in [int]:
             raise TypeError(f"{name} must be an integer")
         elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-
-    def area(self):
-        raise Exception("area() is not implemented")
