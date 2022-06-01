f02e998b9f96a217a8e5f773582fa5379dd0dfc8
David <4292@holbertonschool.com>
2022-06-01 11:41:14 -0400

    fix utf-8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """ Function to read file """
-    with open(filename, encoding="utf=8") as f:
+    with open(filename, encoding="utf-8") as f:
         read_data = f.read()
     print(read_data, end="")
