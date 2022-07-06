a4a03c54e51158f96eaab6fe1cd3329ee18858ac
David <4292@holbertonschool.com>
2022-07-06 14:17:36 -0400

    task 16

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,9 @@
+-- Script that lists all records from second_table of hbnc_0c_0
+-- Query all rows with name value
+SELECT 
+    score,
+    name 
+FROM 
+    second_table
+ORDER BY 
+    score DESC;
