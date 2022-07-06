a2607cbbef3c43ae6eba2f2a9872415c6ddf8d9d
David <4292@holbertonschool.com>
2022-07-06 14:24:25 -0400

    task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,9 +1,12 @@
 -- Script that lists all records from second_table of hbnc_0c_0
--- Query all rows with name value
+-- Query lists all rows with name value, by highest score
 SELECT 
     score,
     name 
 FROM 
     second_table
+WHERE EXISTS(
+    SELECT name FROM second_table
+)
 ORDER BY 
     score DESC;
