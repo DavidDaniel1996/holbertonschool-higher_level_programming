6a8c1ab715e9b6c97d658622ac429faa02903ae5
David <4292@holbertonschool.com>
2022-07-06 13:59:27 -0400

    task 15

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,9 @@
+-- Script lists number of records with same score from second_table of hbnc_0c_0
+-- Query lists scores and times repeated, starting with most repeated
+SELECT
+    score,
+    COUNT(score) AS number
+FROM
+    second_table
+GROUP BY score
+HAVING COUNT(score);
