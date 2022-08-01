63ce2b1c9fde11d8efee05b734412e0da7307dee
David <4292@holbertonschool.com>
2022-08-01 12:15:57 -0400

    fixed sentence

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 let idx = 0;
-const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 while (idx < languages.length) {
   console.log(languages[idx]);
