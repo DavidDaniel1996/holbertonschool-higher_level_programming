22c2ce5c82380d011778b494ca76fbb82cd5fec6
David <4292@holbertonschool.com>
2022-08-01 11:14:28 -0400

    fixed semistandard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -4,6 +4,6 @@ let idx = 0;
 const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 while (idx < languages.length) {
-    console.log(languages[idx]);
-    idx += 1;
+  console.log(languages[idx]);
+  idx += 1;
 }
