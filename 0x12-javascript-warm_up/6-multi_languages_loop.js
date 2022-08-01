40f27c453c6002ce52efeaa857579e45d08a7892
David <4292@holbertonschool.com>
2022-08-01 11:13:33 -0400

    loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+let idx = 0;
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+while (idx < languages.length) {
+    console.log(languages[idx]);
+    idx += 1;
+}
