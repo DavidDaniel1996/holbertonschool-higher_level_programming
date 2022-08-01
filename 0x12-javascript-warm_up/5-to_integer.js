05c9400aba7779447a97e86acb256b7b91a12b43
David <4292@holbertonschool.com>
2022-08-01 12:12:56 -0400

    fixed sentence

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const num = Number(Math.floor(process.argv[2]));
-console.log(isNaN(num) ? 'Not a Number' : `My number: ${num}`);
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
