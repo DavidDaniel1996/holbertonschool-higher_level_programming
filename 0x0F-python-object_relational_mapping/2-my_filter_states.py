0154caa29c5bfcea072e8adc0abb16854372aade
David <4292@holbertonschool.com>
2022-08-06 12:58:11 -0400

    print state with name passed

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module to print rows with Arizona as name"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
+    result = cur.fetchall()
+    print(result)
