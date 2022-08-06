fa08ab2e19659e85c81173fec1e33d6a0ea10809
David <4292@holbertonschool.com>
2022-08-06 12:11:21 -0400

    added safeguard for import

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """Module to print Mysql table"""
 
-import MySQLdb
-import sys
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
 
-db = MySQLdb.connect(host='localhost', user=sys.argv[1],
-                     password=sys.argv[2], database=sys.argv[3])
-cur = db.cursor()
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3])
+    cur = db.cursor()
 
-cur.execute("SELECT * FROM states")
-result = cur.fetchall()
+    cur.execute("SELECT * FROM states")
+    result = cur.fetchall()
 
-for row in result:
-    print(row)
+    for row in result:
+        print(row)
