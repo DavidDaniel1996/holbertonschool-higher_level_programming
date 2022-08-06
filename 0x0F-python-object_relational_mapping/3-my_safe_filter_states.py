14a6973acc05c3f20a5d6383584e4b59b2bf168c
David <4292@holbertonschool.com>
2022-08-06 14:42:09 -0400

    safe_filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module to print rows with specific name"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("""
+            SELECT
+                *
+            FROM
+                states
+            WHERE
+                name = %(state_name)s
+
+        """, {
+            'state_name': sys.argv[4]
+        })
+
+    cur.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
+    result = cur.fetchall()
+
+    for row in result:
+        print(row)
