e9903959f1d98de329ef9923c59af7e87b61a6d9
David <4292@holbertonschool.com>
2022-08-07 14:16:18 -0400

    possible output?

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -16,12 +16,15 @@ if __name__ == "__main__":
     result = session.query(State).all()
     char_match = set('a')
 
+    match = False
     for row in result:
         i = 0
         while i < len(row.name):
             if row.name[i] in char_match:
                 session.delete(row)
+                match = True
                 break
             i += 1
 
-    session.commit()
+    if match is True:
+        session.commit()
