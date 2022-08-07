af19ab9db7a1b3a90ae50cd8a12adabbbfd5366a
David <4292@holbertonschool.com>
2022-08-07 13:12:54 -0400

    change name of state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Lists State objects"""
+
+import sys
+from model_state import Base, State
+
+import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = db.create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Sess = sessionmaker(bind=engine)
+    session = Sess()
+    result = session.query(State).get(2)
+
+    result.name = 'New Mexico'
+
+    session.commit()
