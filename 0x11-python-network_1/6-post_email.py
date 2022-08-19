b9ed4b8593103fd6fad88a65fa28421ea645306d
David <4292@holbertonschool.com>
2022-08-19 17:30:04 -0400

    email

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Sends POST request """
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    email = {'email': sys.argv[2]}
+
+    response = requests.post(sys.argv[1], data=email)
+    print(response.text)
