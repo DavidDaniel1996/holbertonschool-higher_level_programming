cddbfe1f1fa251b34ab5c07a3f3cfb6d7a818e8f
David <4292@holbertonschool.com>
2022-08-19 15:23:00 -0400

    if name

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Module that fetches a url """
 
-import urllib.request
+if __name__ == '__main__':
+    import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as f:
-    body = f.read()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as f:
+        body = f.read()
 
-print('Body Response:')
-print(f"\t- type: {type(body)}")
-print(f"\t- content: {body}")
-print(f"\t- utf content: {body.decode('utf-8')}")
+    print('Body Response:')
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf content: {body.decode('utf-8')}")
