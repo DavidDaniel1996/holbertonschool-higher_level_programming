5eaf45fb4b2f448816829a084d7a1840420f6610
David <4292@holbertonschool.com>
2022-08-19 15:25:14 -0400

    if name

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,7 @@ if __name__ == '__main__':
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as f:
         body = f.read()
-
-    print('Body Response:')
-    print(f"\t- type: {type(body)}")
-    print(f"\t- content: {body}")
-    print(f"\t- utf content: {body.decode('utf-8')}")
+        print('Body Response:')
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf content: {body.decode('utf-8')}")
