253e64d24294c95c1767f961f4267e9f55463f5b
David <4292@holbertonschool.com>
2022-08-19 16:02:50 -0400

    requirement error fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,5 +4,6 @@
 import urllib.request
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as f:
-    print(f.getheader('X-Request-Id'))
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as f:
+        print(f.getheader('X-Request-Id'))
