caae1c56be907955c38b70f3d1119b6551b9f900
David <4292@holbertonschool.com>
2022-08-19 16:01:22 -0400

    trying to fix requirements'

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as f:
         body = f.read()
-        print("Body Response:")
+        print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
