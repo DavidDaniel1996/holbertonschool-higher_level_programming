fa80c07f3b1d932f28817b3a88f3cefddfa594f5
David <4292@holbertonschool.com>
2022-08-19 15:48:09 -0400

    trying to fix requirements

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-""" Module that fetches a url """
-
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
-request = urllib.request.Request('https://intranet.hbtn.io/status')
-with urllib.request.urlopen(request) as f:
-    body = f.read()
-    print('Body Response:')
-    print(f"\t- type: {type(body)}")
-    print(f"\t- content: {body}")
-    print(f"\t- utf content: {body.decode('utf-8')}")
 
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
