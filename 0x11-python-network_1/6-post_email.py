a7c7d818581172ecae5852d49172395aa8dd7d14
David <4292@holbertonschool.com>
2022-08-19 17:45:17 -0400

    aborting

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-""" Sends POST request """
-
+"""Sends POST request"""
 import requests
 import sys
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     url = sys.argv[1]
     email = {"email": sys.argv[2]}
 
