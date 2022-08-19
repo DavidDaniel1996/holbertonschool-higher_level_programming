4827fa16e9a2711aa91b6ac0cfd11c4ff235ab84
David <4292@holbertonschool.com>
2022-08-19 18:04:18 -0400

    error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == '__main__':
     response = requests.get(sys.argv[1])
-    if (response.status_code >= 400):
-        print(response.status_code)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
     else:
         print(response.text)
