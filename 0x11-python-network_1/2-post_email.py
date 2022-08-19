dd00d5a585b9cc4001837d84ae5d69728db9fe08
David <4292@holbertonschool.com>
2022-08-19 16:23:29 -0400

    POST

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ POST requests to url """
 
-from urllib import request, parse
+import urllib.request
+import urllib.parse
 import sys
 
 if __name__ == '__main__':
 
     email = {'email': sys.argv[2]}
 
-    query_string = parse.urlencode(email)
+    query_string = urllib.parse.urlencode(email)
     data = query_string.encode('ascii')
 
-    with request.urlopen(sys.argv[1], data) as response:
+    with urllib.request.urlopen(sys.argv[1], data) as response:
         response_text = response.read()
-        print(response_text)
+        print(response_text.decode('utf8'))
