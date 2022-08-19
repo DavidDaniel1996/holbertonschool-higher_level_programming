5549b417d5fd68c3512d3d22db119a07173c9050
David <4292@holbertonschool.com>
2022-08-19 17:41:02 -0400

    what

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ Sends POST request """
 
+import requests
+import sys
+
 if __name__ == '__main__':
-    import requests
-    import sys
 
-    data = dict(email=sys.argv[2])
+    email = {'email': sys.argv[2]}
 
-    response = requests.post(sys.argv[1], data=data)
+    response = requests.post(sys.argv[1], data=email)
     print(response.text)
