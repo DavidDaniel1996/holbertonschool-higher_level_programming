8d895ee0603e75da72f11d7ac6c3d4c4d37f1c01
David <4292@holbertonschool.com>
2022-08-19 17:44:12 -0400

    r

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = {"email": sys.argv[2]}
 
-    response = requests.post(url, data=email)
-    print(response.text)
+    r = requests.post(url, data=email)
+    print(r.text)
