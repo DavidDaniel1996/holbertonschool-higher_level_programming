b64e33ea3b3cdd38ee683179d09c19f6bb111b19
David <4292@holbertonschool.com>
2022-09-13 14:47:59 -0400

    fixed error code handling

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -8,7 +8,7 @@ function statusCode (response) {
 }
 
 function printError (error) {
-    const code = error.response.status
-    console.log(`code: ${code}`)
+  const code = error.response.status;
+  console.log(`code: ${code}`);
 }
 axios.get(process.argv[2]).then(statusCode).catch(printError);
