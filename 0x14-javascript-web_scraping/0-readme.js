1e001309abb37aec6eb7c696c2f287d851cee1d6
David <4292@holbertonschool.com>
2022-09-13 12:12:46 -0400

    printing error object

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,12 @@
 const fs = require('fs');
 
 function readData (data) {
-  console.log(data);
-}
+    try {
+        console.log(data);
+    }
+    catch(err) {
+        console.log(err.message);
+    }
+};
 
 fs.readFile(process.argv[2], 'utf-8', readData);
