ffb08837f41e53adb7b3253467b120998f950ad1
David <4292@holbertonschool.com>
2022-09-13 20:59:18 -0400

    Finally, after 4 hours, matching Wedge, and actually being able to print it

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+const starWars = process.argv[2];
+let counter = 0;
+
+axios.get(starWars).then(function (response) {
+  for (let i = 0; i < response.data.count; i++) {
+    for (let j = 1; j < response.data.results[i].characters.length; j++) {
+      if (response.data.results[i].characters[j].includes('18')) {
+        counter = counter + 1;
+      }
+    }
+  }
+  return (counter);
+}).then(console.log);
