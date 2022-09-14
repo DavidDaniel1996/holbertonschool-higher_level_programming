fd6b416e86f7f24ac4e1253e4d0f95c65fab447c
David <4292@holbertonschool.com>
2022-09-14 11:28:10 -0400

    fixing bug

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,15 +6,10 @@ const url = process.argv[2];
 axios.get(url).then(function (response) {
   const tasksCompleted = {};
   let userID = 0;
-  let count = 0;
   for (let i = 0; i < response.data.length; i++) {
     userID = response.data[i].userId;
-    if (!(userID in tasksCompleted)) {
-      count = 0;
-    }
     if (response.data[i].completed === true) {
-      count++;
-      tasksCompleted[userID] = count;
+      tasksCompleted[userID] = (tasksCompleted[userID] || 0) + 1;
     }
   }
   console.log(tasksCompleted);
