1937ebf12c2fb498e90142a56784b43d8d95b16d
David <4292@holbertonschool.com>
2022-09-30 13:39:02 -0400

    fixing comment

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,29 +1,11 @@
 #!/usr/bin/python3
-"""
-Module: 1-my_list
-Class that inherets
-from 'list'
-"""
+""" Module contains class MyList, inherets list """
 
 
 class MyList(list):
-    """
-    Class for organizing
-    lists.
-
-    Arguments:
-        - list: Class List
-    Attributes:
-    ====================
-    Public Instance Method:
-        - def print_sorted(self)
-    """
+    """ Defines MyList """
 
     def print_sorted(self):
-        """
-        Prints our list in
-        ascending order
-        """
         sorted_list = self[:]
         sorted_list.sort()
         print(sorted_list)
