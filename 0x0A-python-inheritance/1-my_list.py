229a527e4dbe15922d61be52c21bb2b272ee8efe
David <4292@holbertonschool.com>
2022-09-30 13:37:35 -0400

    trying out this comment

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
-""" Module contains class MyList, inherets list """
+"""
+Module: 1-my_list
+Class that inherets
+from 'list'
+"""
 
 
 class MyList(list):
-    """ Defines MyList """
+    """
+    Class for organizing
+    lists.
+
+    Arguments:
+        - list: Class List
+    Attributes:
+    ====================
+    Public Instance Method:
+        - def print_sorted(self)
+    """
+
     def print_sorted(self):
+        """
+        Prints our list in
+        ascending order
+        """
         sorted_list = self[:]
         sorted_list.sort()
         print(sorted_list)
